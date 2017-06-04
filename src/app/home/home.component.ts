f6f2345375e1785ab098d82dd5d5881cd3eeaad1
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-04 18:51:51 +0100

    add the lazy loaded HomeComponent

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded HomeComponent
+ * 
+ * @export
+ * @class HomeComponent
+ * @implements {OnInit}
+ */
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

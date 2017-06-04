c230b99ec9a467feec4a6deb2c7ccb82ac59e280
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-04 18:53:47 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { APP_BASE_HREF } from "@angular/common";
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+
+import { SharedModule } from "./shared/shared.module";
+
+import { HomeModule } from "./home/home.module";
+import { LoginModule } from "./login/login.module";
 
 @NgModule({
   declarations: [
@@ -12,9 +19,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    SharedModule,
+    HomeModule,
+    LoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_BASE_HREF,
+    useValue: '/'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
